816eef714bf0bbc673d02c70a032a7336bf31755
TusharD-047 <56781715+TusharD-047@users.noreply.github.com>
2020-03-31 20:28:53 +0530

    dekh lena

---
+++ app/src/main/java/com/example/my_dash/Departmental.java
@@ -0,0 +1,25 @@
+package com.example.my_dash;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Departmental extends Fragment {
+
+    View view;
+    public Departmental()  {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.departmental,container,false);
+        return view;
+    }
+}
