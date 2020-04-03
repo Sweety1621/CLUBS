b293373f8d5c5c19d219f3bb1e265e5d7ae10c60
Sweety1621 <aditiranjan16@gmail.com>
2020-04-04 01:53:05 +0530

    aaaaaaaaaaaaaaaa

--- app/src/main/java/com/example/my_dash/models/DepClubs.java
+++ app/src/main/java/com/example/my_dash/models/DepClubs.java
@@ -8,20 +8,91 @@ public class DepClubs {
             {".EXE",
                     "Overview",
                     "https://scontent.fpat3-1.fna.fbcdn.net/v/t1.0-9/48376249_2181062101914579_6963114676649459712_n.jpg?_nc_cat=110&_nc_sid=dd9801&_nc_ohc=KCkSUvuuPaQAX-lthce&_nc_ht=scontent.fpat3-1.fna&oh=b9e2593d93c6c1d9803ac6b7e1f1e5e7&oe=5EAA2604",
-                    "Team .EXE,the departmental team representing the Computer Science and Engineering Department at the Annual Technical Extravaganza of NIT Hamirpur, Nimbus.",
+                    "Team .EXE,the departmental team representing the Computer Science and Engineering Department at the Annual Technical Extravaganza of NIT Hamirpur, Nimbus.Each year we strive to raise the bar for technical advancement and showcasing the best of Computer Science and Software Development for anyone to see. When it comes to projects, we take up projects that are helpful to the students and thus result in a smooth life for the students in the college; and we finish them using a corporate protocol of deadlines and online version control. Not only this, we organize many events on the actual three days of Nimbus, which are aimed to provide a fun-filled but still quite informative experience of what Computer Science has to offer. These events range from an online competitive coding competition to an offline debugging race, and a gaming competition too.",
+                    "Professional as well as Personal growth.Achieve excellence in Teamwork.",
+                    "We made an app Paradox, available on Play store that has over 2000 downloads.The Online open elective portal made by team .Exe will be implemented in the college soon.",
                     },
             {
                 "C-HELIX",
                      "Overview",
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fc-helix&psig=AOvVaw091or4kmat2Wos-G8-GQMz&ust=1586027598342000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCN-bD7zOgCFQAAAAAdAAAAABAD",
+                    "Team C-Helix represents an insight view of Civil department that has been working in NIT Hamirpur from a long way.NIMBUS, The Techfest of our college allows students from various stream to show their technicality in every field of science. Basically, it’s a student body which explores their creativity and develops meaningful projects. Every year, we make new and innovative projects and show them in an exhibition. Also, we organize certain technical events in which students, as well as teachers, participate to engage their skills and practical knowledge. Educational Workshops and guest lectures are also conducted where students acquire software skills and develop their personality. But as we all know that a happy mind can work better and effective. So, at the end of the Fest, we along with other students take part in the fun event organized by us.    ",
+                    " ",
                     "",
-                    ""
             },
             {
                 "CSEC",
-                    "  ",
+                    "Overview",
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fcsec.nith%2F&psig=AOvVaw1Bh-gaqa0wbWgilVo7Dr_c&ust=1586027725065000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJC86-77zOgCFQAAAAAdAAAAABAD",
+                    "Computer Science Engineers Community CSEC is the community of computer science enthusiasts of NITH, aimed at lending a helping hand to all the beginners in the world of programming.",
                     "",
-                    ""
+                    " ",
+
             },
+            {
+               "CSOC" ,
+                    "Overview",
+                    "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.csoc.in%2F&psig=AOvVaw3vzgPAw7ajLoxZ3cbbxpLr&ust=1586027793133000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPis44_8zOgCFQAAAAAdAAAAABAD",
+                    "",
+                    "",
+            },
+            {
+                "DESIGN O CRATS",
+                    "Overview",
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fdoc.arch.nimbus%2Fposts%2F&psig=AOvVaw1TPgHCCf5nov5yRphPnXwL&ust=1586027946810000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMiu2tn8zOgCFQAAAAAdAAAAABAD",
+                    "",
+                    "",
+            },
+            {
+                    "DRAVYA",
+                    "Overview",
+                    "https://scontent.fpat3-1.fna.fbcdn.net/v/t1.0-9/p960x960/43505697_1949289108483508_3893313009723899904_o.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=JpaiS3fVc_kAX-tiptb&_nc_ht=scontent.fpat3-1.fna&_nc_tp=6&oh=d3e8bf7ccdd430320ac4df0f10d301f8",
+                    "",
+                    "",
+            },
+            {
+                    "HERMETICA",
+                    "Overview",
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fnith.ac.in%2Fwp-content%2Fuploads%2F2016%2F10%2Fal15.pdf&psig=AOvVaw2DUZeNeGEhUak9BOo62Isq&ust=1586028206928000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMCl0Nb9zOgCFQAAAAAdAAAAABAD",
+                    "",
+                    "",
+            },
+            {
+                    "MEDextrous",
+                    "Overview",
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fmedextrous%2F&psig=AOvVaw1TPgHCCf5nov5yRphPnXwL&ust=1586027946810000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMiu2tn8zOgCFQAAAAAdAAAAABAN",
+                    "",
+                    "",
+            },
+            {
+                    "META MORPH",
+                    "Overview",
+                    "",
+                    "",
+                    "",
+            },
+            {
+                    "OJAS",
+                    "Overview",
+                    "",
+                    "",
+                    "",
+            },
+            {
+                    "SPEC",
+                    "Overview",
+                    "",
+                    "",
+                    "",
+            },
+            {
+                    "VIBHAV",
+                    "Overview",
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fteamvibhavece%2F&psig=AOvVaw1TPgHCCf5nov5yRphPnXwL&ust=1586027946810000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMiu2tn8zOgCFQAAAAAdAAAAABAI",
+                    "",
+                    "",
+            },
+
 
 
     };
